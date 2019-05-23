f7ef911f8bfe7bddc76e99c8b107af8e6b5009fe
Mitchell Anderson <Mitchell.Anderson@capturis.com>
2019-05-23 10:25:40 -0500

    Finished application

--- boardgameapp/main.js
+++ boardgameapp/main.js
@@ -1,10 +1,7 @@
 const container = document.getElementById("container");
-
-
-
 const clientId  = 'M37xARRD2X';
-
 const catanSearchUrl = 'https://www.boardgameatlas.com/api/search?name=Catan&client_id=' + clientId;
+
 
 async function getData() {
     try {
@@ -22,37 +19,38 @@ async function getData() {
     }
 }
 
+
 function displayData(jsonResponse) {
+    container.innerHTML = ``;
+
     if(!jsonResponse.length) {
-        container.innerHTML=`<h3>No games were found<h3>`;
+        container.innerHTML = `<h3>No games were found<h3>`;
     }
 
     for(let i = 0; i < jsonResponse.length; i++) {
-        let {gameTitle, releaseDate, nPlayers, img, desc} = parseResponse(jsonResponse[i]);
-
         container.innerHTML += `
-            <div class="gameInfo">
-                <h3 class="title">` + gameTitle + `</h3>
+            <div id="gameInfo">
+                <h3 id="title">` + jsonResponse[i].name + `</h3>
         
-                <div class="date">
-                    ` + releaseDate + `
+                <div id="date">
+                    ` + (jsonResponse[i].year_published != null ? jsonResponse[i].year_published : 'Year unknown') + `
                 </div>
-        
-                <div class="players">
-                    ` + nPlayers + `
+                
+                <div id = "players" >
+                    ` + (jsonResponse[i].min_players != null ?
+                            jsonResponse[i].min_players + ` -  ` + jsonResponse[i].max_players + ` players ` :
+                            '<br>') + `
                 </div>
+                
+                <img id="gameImg" src="` + jsonResponse[i].image_url + `" />
         
-                <img class="gameImg" src="` + img + `" />
-        
-                <div class="description">
-                    ` + desc + `
+                <div id="description">
+                    ` + jsonResponse[i].description + `
                 </div>
+                
+                <a id="link" href="` + jsonResponse[i].url + `">Board Game Atlas Page</a>
             </div> `;
     }
-}
-
-function parseResponse() {
-    return {gameTitle: "Catan", releaseDate: "11/11/1111", nPlayers: "3-4 Players", img: "https://images-na.ssl-images-amazon.com/images/I/51Eiofu9mqL.jpg", desc: "Description here."}
 }
 
 getData();
